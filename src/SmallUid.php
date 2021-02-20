e8cbf6b894d55cd1bad812a58bb9ed4834770fcf
Bruce <bruce@mediagone.fr>
2021-02-20 12:13:45 +0100

    New "SmallUid" value object + tests

---
+++ src/SmallUid.php
@@ -0,0 +1,237 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Common\SmallUid;
+
+use DateTimeImmutable;
+use InvalidArgumentException;
+use Mediagone\Common\Types\Text\Hex;
+use Mediagone\Common\Types\ValueObject;
+use function base_convert;
+use function explode;
+use function gmp_init;
+use function gmp_strval;
+use function is_string;
+use function microtime;
+use function preg_match;
+use function str_pad;
+use function strrev;
+use function substr;
+
+
+class SmallUid implements ValueObject
+{
+    //========================================================================================================
+    // Constants
+    //========================================================================================================
+    
+    public const TOTAL_CHARS_LENGTH = 16;
+    
+    public const RANDOM_CHARS_LENGTH = 5;
+    
+    
+    
+    //========================================================================================================
+    // Properties
+    //========================================================================================================
+    
+    private Hex $hex;
+    
+    private string $value;
+    
+    
+    
+    //========================================================================================================
+    // Constructors
+    //========================================================================================================
+    
+    private function __construct(Hex $hex)
+    {
+        if (! self::isHexValid($hex)) {
+            throw new InvalidArgumentException(static::class . ' can only be created from a '. self::TOTAL_CHARS_LENGTH .' chars long Hex (got ' . $hex->getLength() . ' chars).');
+        }
+        
+        $this->hex = $hex;
+        $this->value = self::hexToBase62($hex->toString());
+    }
+    
+    
+    /**
+     * Creates a new instance with nil value.
+     */
+    final public static function nil()
+    {
+        return new static(Hex::fromString('0000000000000000'));
+    }
+    
+    
+    /**
+     * Creates a new instance with a sequential random value.
+     */
+    final public static function random()
+    {
+        $hex = self::generateHexTimestamp() . self::generateHexRandomBytes();
+        
+        return new static(Hex::fromString($hex));
+    }
+    
+    
+    /**
+     * Creates a new instance from the given Hex object.
+     */
+    final public static function fromHex(Hex $hex)
+    {
+        return new static($hex);
+    }
+    
+    
+    /**
+     * Creates a new instance from the given base62 string representation.
+     */
+    final public static function fromString(string $base62string)
+    {
+        if (! self::isValueValid($base62string)) {
+            throw new InvalidArgumentException('Invalid base62 string (' . $base62string . ') for ' . static::class . ', it must only contains a-z or 0-9 chars."');
+        }
+        
+        return new static(Hex::fromString(self::base62ToHex($base62string)));
+    }
+    
+    
+    
+    //========================================================================================================
+    // Static methods
+    //========================================================================================================
+    
+    /**
+     * Returns whether the specified value is a valid base62 string respresentation.
+     *
+     * @var string $value
+     */
+    public static function isValueValid($value) : bool
+    {
+        if (! is_string($value)) {
+            return false;
+        }
+        
+        if (preg_match('#^[a-z0-9]+$#i', $value) !== 1) {
+            return false;
+        }
+        
+        $hex = Hex::fromString(self::base62ToHex($value));
+        
+        return self::isHexValid($hex);
+    }
+    
+    
+    
+    //========================================================================================================
+    // Methods
+    //========================================================================================================
+    
+    final public function jsonSerialize()
+    {
+        return $this->value;
+    }
+    
+    
+    /**
+     * Return the uid's base62 string representation.
+     */
+    final public function toString() : string
+    {
+        return $this->value;
+    }
+    
+    
+    /**
+     * Returns the uid's underlying Hex string.
+     */
+    final public function toHex() : Hex
+    {
+        return $this->hex;
+    }
+    
+    
+    
+    /**
+     * Returns the uid's timestamp as a date time object (without milliseconds).
+     */
+    final public function getDatetime() : DateTimeImmutable
+    {
+        $timestampLength = self::TOTAL_CHARS_LENGTH - self::RANDOM_CHARS_LENGTH;
+        $baseTimestamp = (string)hexdec(substr($this->hex->toString(), 0, $timestampLength));
+        
+        $seconds = (int)substr($baseTimestamp, 0, - 3);
+        
+        return (new DateTimeImmutable())->setTimestamp($seconds);
+    }
+    
+    
+    /**
+     * Compares the uid to an other uid, the result is TRUE if both contain the same value.
+     */
+    final public function isEqualTo(SmallUid $other) : bool
+    {
+        return $this->value === $other->value;
+    }
+    
+    
+    
+    //========================================================================================================
+    // Helpers
+    //========================================================================================================
+    
+    private static function isHexValid(Hex $hex) : bool
+    {
+        return $hex->getLength() === self::TOTAL_CHARS_LENGTH;
+    }
+    
+    
+    /**
+     * Returns the current timestamp as a 11 chars long hexadecimal string, with a 3 decimals precision (10^-3 s).
+     * Note: "seconds" will overflow in 2038 if ran on 32bit PHP, then ever deploy on a 64bit platform!
+     */
+    private static function generateHexTimestamp() : string
+    {
+        $time = microtime(false);
+        [$microseconds, $seconds] = explode(' ', $time);
+        
+        // Keep only millisecond digits.
+        $microseconds = substr($microseconds, 2,3);
+        
+        $hex = base_convert($seconds . $microseconds, 10, 16);
+        
+        return str_pad($hex, self::TOTAL_CHARS_LENGTH - self::RANDOM_CHARS_LENGTH, '0', STR_PAD_LEFT);
+    }
+    
+    
+    private static function generateHexRandomBytes() : string
+    {
+        $randomHex = Hex::random(self::RANDOM_CHARS_LENGTH);
+        
+        return str_pad($randomHex->toString(), self::RANDOM_CHARS_LENGTH, '0', STR_PAD_LEFT);
+    }
+    
+    
+    private static function hexToBase62(string $hex) : string
+    {
+        // Reverse the hex string to maximize output's randomness (prevent similar prefixes).
+        $hex = strrev($hex);
+        
+        return gmp_strval(gmp_init($hex, 16), 62);
+    }
+    
+    
+    private static function base62ToHex(string $base62string) : string
+    {
+        $hex = gmp_strval(gmp_init($base62string, 62), 16);
+        
+        // Reverse back the hex string, and pad it to restore terminal zeros.
+        $hex = str_pad(strrev($hex), self::TOTAL_CHARS_LENGTH, '0', STR_PAD_RIGHT);
+        
+        return $hex;
+    }
+    
+    
+    
+}
---
+++ tests/SmallUidTest.php
@@ -0,0 +1,189 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Mediagone\Common\SmallUid;
+
+use DateTime;
+use InvalidArgumentException;
+use Mediagone\Common\SmallUid\SmallUid;
+use Mediagone\Common\Types\Text\Hex;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \Mediagone\Common\SmallUid\SmallUid
+ */
+final class SmallUidTest extends TestCase
+{
+    //========================================================================================================
+    // Instantiation
+    //========================================================================================================
+    
+    public function test_can_create_a_nil_uid() : void
+    {
+        self::assertInstanceOf(SmallUid::class, SmallUid::nil());
+    }
+    
+    
+    public function test_can_create_a_random_uid() : void
+    {
+        self::assertInstanceOf(SmallUid::class, SmallUid::random());
+    }
+    
+    
+    public function test_can_be_created_from_a_valid_string() : void
+    {
+        $uid = SmallUid::fromString('LscmjzUyKLR');
+        self::assertSame('LscmjzUyKLR', $uid->toString()); //1234567890abcdef
+    }
+    
+    
+    public function test_cannot_be_created_from_an_invalid_string() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        
+        SmallUid::fromString('');
+    }
+    
+    public function test_can_be_created_from_an_Hex() : void
+    {
+        $hex = Hex::fromString('1234567890abcdef');
+        $uid = SmallUid::fromHex($hex);
+        
+        self::assertInstanceOf(SmallUid::class, $uid);
+        self::assertSame('LscmjzUyKLR', $uid->toString());
+        self::assertSame($hex->toString(), $uid->toHex()->toString());
+    }
+    
+    
+    public function test_cannot_be_created_from_a_too_short_Hex() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        
+        SmallUid::fromHex(Hex::fromString('1234567890abc'));
+    }
+    
+    
+    public function test_cannot_be_created_from_a_too_long_Hex() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        
+        SmallUid::fromHex(Hex::fromString('1234567890abcdefFFFF'));
+    }
+    
+    
+    
+    //========================================================================================================
+    // Conversion
+    //========================================================================================================
+    
+    public function test_can_be_encoded_to_json() : void
+    {
+        $uid = SmallUid::fromString('LscmjzUyKLR');
+        
+        self::assertSame('"LscmjzUyKLR"', json_encode($uid->jsonSerialize()));
+    }
+    
+    
+    public function test_can_be_cast_to_string() : void
+    {
+        $uid = SmallUid::fromString('LscmjzUyKLR');
+        
+        self::assertSame('LscmjzUyKLR', $uid->toString());
+    }
+    
+    
+    public function test_can_be_returned_as_string() : void
+    {
+        $uid = SmallUid::fromString('LscmjzUyKLR');
+        
+        self::assertSame('LscmjzUyKLR', $uid->toString());
+    }
+    
+    
+    public function test_can_be_returned_as_Hex() : void
+    {
+        $uid = SmallUid::fromString('LscmjzUyKLR');
+        
+        self::assertInstanceOf(Hex::class, $uid->toHex());
+        self::assertSame('1234567890abcdef', $uid->toHex()->toString());
+    }
+    
+    
+    public function test_can_get_creation_datetime() : void
+    {
+        $now = new DateTime();
+        $timestamp = str_pad((string)$now->getTimestamp(), 11, '0', STR_PAD_LEFT);
+        $timestamp .= '000'; // add milliseconds
+        
+        $timestampPart = base_convert($timestamp, 10, 16);
+        $randomPart = 'AAAAA';
+        $uid = SmallUid::fromHex(Hex::fromString($timestampPart . $randomPart));
+        
+        self::assertSame($now->format('Y-m-d H:i:s'), $uid->getDatetime()->format('Y-m-d H:i:s'));
+    }
+    
+    
+    public function test_nil_uid_is_valid() : void
+    {
+        self::assertSame('0', SmallUid::nil()->toString());
+        self::assertSame('0000000000000000', SmallUid::nil()->toHex()->toString());
+    }
+    
+    
+    
+    //========================================================================================================
+    // Misc
+    //========================================================================================================
+    
+    public function test_can_tell_value_is_valid() : void
+    {
+        self::assertTrue(SmallUid::isValueValid('LscmjzUyKLR'));
+    }
+    
+    
+    public function test_can_tell_too_long_value_is_invalid() : void
+    {
+        self::assertFalse(SmallUid::isValueValid('LscmjzUyKLRaaaaaaaaaaaaaa'));
+    }
+    
+    
+    public function test_can_tell_empty_value_is_invalid() : void
+    {
+        self::assertFalse(SmallUid::isValueValid(''));
+    }
+    
+    
+    public function test_can_tell_non_string_value_is_invalid() : void
+    {
+        self::assertFalse(SmallUid::isValueValid(100));
+        self::assertFalse(SmallUid::isValueValid(true));
+    }
+    
+    
+    public function test_can_compare_two_equal_uids() : void
+    {
+        $uid1 = SmallUid::fromString('LscmjzUyKLR');
+        $uid2 = SmallUid::fromString('LscmjzUyKLR');
+        self::assertTrue($uid1->isEqualTo($uid2));
+        self::assertTrue($uid2->isEqualTo($uid1));
+    }
+    
+    
+    public function test_can_compare_two_different_uids() : void
+    {
+        $uid1 = SmallUid::fromString('LscmjzUyKLR');
+        $uid2 = SmallUid::fromString('DifferentId');
+        self::assertFalse($uid1->isEqualTo($uid2));
+        self::assertFalse($uid2->isEqualTo($uid1));
+    }
+    
+    
+    public function test_can_be_extended() : void
+    {
+        self::assertInstanceOf(SmallUid::class, ExtendedSmallUid::nil());
+        self::assertInstanceOf(ExtendedSmallUid::class, ExtendedSmallUid::nil());
+    }
+    
+    
+    
+}
